a106ee79c5dd807ab97952c20571f3a7791151d9
Matthew Molloy <whamtet@gmail.com>
2021-07-12 08:48:40 +0900

    init commit

---
+++ wp2/Program.cs
@@ -0,0 +1,59 @@
+﻿using System.IO;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using clojure.lang;
+
+namespace PeachPied.Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Directory.SetCurrentDirectory(Path.GetDirectoryName(typeof(Program).Assembly.Location));
+
+            //
+            var host = WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>()
+                .UseUrls("http://*:5004/")
+                .Build();
+
+            IFn load = clojure.clr.api.Clojure.var("clojure.core", "load-string");
+            load.invoke("(clojure.core.server/start-server {:port 5555 :name \"repl\" :accept 'clojure.core.server/repl})" +
+                        "(println \"serving!\")");
+
+            host.Run();
+        }
+    }
+
+    class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+            services.AddResponseCompression();
+            services.AddWordPress(options =>
+            {
+                // options.SiteUrl =
+                // options.HomeUrl = "http://localhost:5004";
+
+                // options.PluginContainer.Add(new DashboardPlugin());
+            });
+        }
+
+        public void Configure(IApplicationBuilder app, IHostEnvironment env, IConfiguration configuration)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseWordPress();
+
+            app.UseDefaultFiles();
+        }
+    }
+}
